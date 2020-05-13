3c3dffcd3a7bc9d78e5e5fa1bdc3cfb81532c2c6
YukiAkase <y.akase.k@gmail.com>
2020-05-13 23:19:30 +0900

    リストを修飾

--- src/components/todos/index.tsx
+++ src/components/todos/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../stores';
 import { completeTodo, deleteTodo } from '../../stores/todo';
-import { IconButton } from '@material-ui/core';
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Checkbox,
+  Avatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
+import CreateIcon from '@material-ui/icons/Create';
 
 export default function Todos() {
   const todoList = useSelector((state: RootState) => state.todo);
@@ -11,25 +21,31 @@ export default function Todos() {
   const dispatch = useDispatch();
 
   return (
-    <ul>
+    <List dense={false}>
       {todoList.todos.map((todo) => (
-        <li key={todo.id}>
-          <input
-            type="checkbox"
+        <ListItem key={todo.id}>
+          <ListItemAvatar>
+            <Avatar>
+              <CreateIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <Checkbox
             name={todo.id.toString()}
             id={todo.id.toString()}
             onClick={() => dispatch(completeTodo(todo.id))}
             defaultChecked={todo.isComplete}
           />
-          {todo.title}
-          <IconButton
-            aria-label="delete"
-            onClick={() => dispatch(deleteTodo(todo.id))}
-          >
-            <DeleteIcon />
-          </IconButton>
-        </li>
+          <ListItemText primary={todo.title}></ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton
+              aria-label="delete"
+              onClick={() => dispatch(deleteTodo(todo.id))}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
