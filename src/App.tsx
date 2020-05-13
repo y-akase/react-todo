9b844c596330c9913fce8bc4d91909167ee2c279
YukiAkase <y.akase.k@gmail.com>
2020-05-13 22:19:26 +0900

    コンテンツをContainerでラップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import AddField from './components/add-field';
 import Todos from './components/todos';
-import { Box } from '@material-ui/core';
+import { Box, Container } from '@material-ui/core';
 
 interface IPropsApp {}
 interface IStateApp {}
 export default class App extends React.Component<IPropsApp, IStateApp> {
   render() {
     return (
-      <Box textAlign="center">
-        <AddField />
-        <Todos />
-      </Box>
+      <Container maxWidth="sm">
+        <Box textAlign="center">
+          <AddField />
+          <Todos />
+        </Box>
+      </Container>
     );
   }
 }
